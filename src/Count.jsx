7ea409736770d37473c838c777e35909defe73cb
New_Sample_Website <saiful66261@gmail.com>
2026-04-09 16:35:24 +0530

    new file

--- src/Count.jsx
+++ src/Count.jsx
@@ -8,12 +8,18 @@ const Count = () => {
             setCount((prev)=> prev + 1)
         }
     }
+    const handleDecrease = () => {
+        if(count > 0) {
+            setCount((prev)=> prev - 1)
+        }
+    }
     return (
         <div>
             <h3 className="bg-linear-90 to-blue-600 via-orange-700 from-purple-700 bg-clip-text" style={{WebkitTextFillColor: "transparent", WebkitBackgroundClip: "text", fontSize: "30px", fontWeight: "700"}}>InDe Operation</h3>
             <p className="text-5xl font-bold my-[20px] text-center">{count}</p>
             <div className="flex gap-3 justify-center items-center">
                <ButtonComponents onclick={handleIncrement} type="button" text="+" className="bg-blue-600 font-bold text-white"/>
+               <ButtonComponents type="button" text="-" className="bg-green-600 font-bold" onclick={handleDecrease}/>
             </div>
         </div>
     )
